bfdecfe477a8e4f20b499cba238254274439b430
grzesiu <gchlebus@gmail.com>
2015-01-16 01:05:05 +0100

    Implemented GameObject class.

--- LifeUniversalisProject/LifeUniversalis/GameObject.cpp
+++ LifeUniversalisProject/LifeUniversalis/GameObject.cpp
@@ -1,6 +1,18 @@
 #include "GameObject.h"
 #include "GameObjectComponent.h"
 
+GameObject::~GameObject()
+{
+	for (size_t i = 0; i < _components.size(); ++i)
+	{
+		_components[i]->onDetachFromParent();
+		_components[i]->detachFromParent();
+		_components[i]->onDelete();
+		delete _components[i];
+	}
+	_components.clear();
+}
+
 void GameObject::start()
 {
 	for (size_t i = 0; i < _components.size(); ++i)
@@ -26,6 +38,7 @@ bool GameObject::addComponent(GameObjectComponent *component)
 	{
 		_components.push_back(component);
 		component->attachToParent(this);
+		component->onAttachToParent();
 		wasAdded = true;
 	}
 
@@ -34,5 +47,69 @@ bool GameObject::addComponent(GameObjectComponent *component)
 
 GameObjectComponent* GameObject::findComponent(const std::string &componentName)
 {
-	return nullptr;
+	// call the const version and then cast away the constness of the return type
+	const GameObject *thisInstance = this;
+	const GameObjectComponent *comp = thisInstance->findComponent(componentName);
+	return const_cast<GameObjectComponent*>(comp);
+}
+
+const GameObjectComponent* GameObject::findComponent(const std::string &componentName) const
+{
+	GameObjectComponent *comp = nullptr;
+
+	size_t idx = 0;
+	if (_getComponentIndex(componentName, idx))
+	{
+		comp = _components[idx];
+	}
+
+	return comp;
+}
+
+GameObjectComponent* GameObject::removeComponent(const std::string &componentName)
+{
+	GameObjectComponent *comp = nullptr;
+
+	size_t idx = 0;
+	if (_getComponentIndex(componentName, idx))
+	{
+		comp = _components[idx];
+		comp->onDetachFromParent();
+		comp->detachFromParent();
+		_components.erase(_components.begin() + idx);
+	}
+
+	return comp;
+}
+
+bool GameObject::deleteComponent(const std::string &componentName)
+{
+	bool wasDeleted = false;
+
+	GameObjectComponent *comp = removeComponent(componentName);
+	if (comp)
+	{
+		comp->onDelete();
+		delete comp;
+	}
+
+	return wasDeleted;
+}
+
+bool GameObject::_getComponentIndex(const std::string &componentName, size_t &idx) const
+{
+	bool wasFound = false;
+
+	for (size_t i = 0; i < _components.size(); ++i)
+	{
+		const GameObjectComponent *comp = _components[i];
+		if (comp->getName() == componentName)
+		{
+			wasFound = true;
+			idx = i;
+			break;
+		}
+	}
+
+	return wasFound;
 }
--- LifeUniversalisProject/LifeUniversalis/GameObject.h
+++ LifeUniversalisProject/LifeUniversalis/GameObject.h
@@ -11,10 +11,24 @@ public:
 	~GameObject();
 	void start();
 	void update();
+	
+	//! \return True if component was added, false otherwise.
 	bool addComponent(GameObjectComponent *component);
+    
+	//! \return Pointer to found component, null if component was not found.
 	GameObjectComponent* findComponent(const std::string &componentName);
+	const GameObjectComponent* findComponent(const std::string &componentName) const;
+	
+	//! \return Pointer to removed component, null if component was not found.
+	GameObjectComponent* removeComponent(const std::string &componentName);
+	
+	//! \return True if component was deleted, false otherwise.
 	bool deleteComponent(const std::string &componentName);
 
 private:
+	//! Gets component index in internal component vector.
+	//! \return True if idx is valid, false otherwise.
+	bool _getComponentIndex(const std::string &componentName, size_t &idx) const;
+
 	std::vector<GameObjectComponent*> _components;
 };
