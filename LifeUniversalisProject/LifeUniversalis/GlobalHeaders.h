1601b6b4297f5c29072eecf211bcc56f66d2ca90
grzesiu <gchlebus@gmail.com>
2015-01-24 13:23:26 +0100

    Added geometric typedefs

--- LifeUniversalisProject/LifeUniversalis/GlobalHeaders.h
+++ LifeUniversalisProject/LifeUniversalis/GlobalHeaders.h
@@ -1,3 +1,4 @@
+#pragma once
 
 #include "LifeUniversalisSystem.h"
 
@@ -6,3 +7,11 @@
 #include <algorithm>
 #include <boost\thread.hpp>
 #include <boost\algorithm\clamp.hpp>
+#include <Eigen\Core>
+#include <Eigen\Geometry>
+
+typedef Eigen::Vector2f Vector2;
+typedef Eigen::Vector3f Vector3;
+typedef Eigen::Matrix3f Matrix3;
+typedef Eigen::Matrix4f Matrix4;
+typedef Eigen::Quaternionf Quaternion;
