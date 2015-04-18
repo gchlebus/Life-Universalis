c86f2ee822457703002461a3f906819349ffe81a
gchlebus <gchlebus@gmail.com>
2015-04-18 18:03:26 +0200

    Cleaned up WorkHumanTask.

--- Game/Game/WorkHumanTask.cpp
+++ Game/Game/WorkHumanTask.cpp
@@ -14,68 +14,50 @@
 #include "HumanComponentNames.h"
 
 
-WorkHumanTask::~WorkHumanTask()
+std::string WorkHumanTask::getTaskName()
 {
+    return HT_WORK;
 }
 
 void WorkHumanTask::onUpdate()
 {
-    HumanInteraction *currentInteraction = _humanComponent->humanInteraction->getCurrentInteraction();
-    if(getState() == EXECUTING)
+    HumanInteraction* currentInteraction = _humanComponent->humanInteraction->getCurrentInteraction();
+    if (getState() == EXECUTING)
     {
-        if(currentInteraction == nullptr)
+        if (currentInteraction == nullptr)
         {
             terminateGracefully();
         }
     }
-    if(getState() == TERMINATING)
+    if (getState() == TERMINATING)
     {
-        if(currentInteraction == nullptr)
+        if (currentInteraction == nullptr)
             finish();
     }
 }
 
 Vector3 WorkHumanTask::goTarget()
 {
-    if(_humanComponent->getWorkplace() == nullptr)
+    if (_humanComponent->getWorkplace() == nullptr)
         return Vector3(0, 0, 0);
     return _humanComponent->getWorkplace()->parent->getParent()->getTransform().getWorldPosition();
 }
 
-std::string WorkHumanTask::getTaskName()
-{
-    return HT_WORK;
-}
-
-//void humanTaskDidExecute(HumanTask* task);
-//
-//void humanTaskWillGoToTarget(HumanTask* task);
-//void humanTaskDidReachTarget(HumanTask* task);
-//
-//void humanTaskWillInteract(HumanTask* task);
-//void humanTaskDidInteract(HumanTask* task);
-//
-//void humanTaskDidPause(HumanTask* task);
-//void humanTaskDidResume(HumanTask* task);
-//
-//void humanTaskWillTerminateGracefully(HumanTask* task);
-//void humanTaskWillTerminateImmediately(HumanTask* task);
-//void humanTaskDidTerminate(HumanTask* task);
-
 void WorkHumanTask::humanTaskWillTerminateImmediately(HumanTask* task)
 {
-    if(_humanComponent->humanInteraction->getCurrentInteraction() != nullptr)
+    if (_humanComponent->humanInteraction->getCurrentInteraction() != nullptr)
         _humanComponent->humanInteraction->getCurrentInteraction()->terminateImmediately();
 }
+
 void WorkHumanTask::humanTaskDidTerminate(HumanTask* task)
 {
-    ((DayTimeEntity*)GameEngine::engine()->currentEnvironment->findEntity(EN_DAYTIME))->showOutputNextFrame();
+    ((DayTimeEntity*) GameEngine::engine()->currentEnvironment->findEntity(EN_DAYTIME))->showOutputNextFrame();
     LOG("WORK HAS BEEN TERMINATED!");
 }
 
-void WorkHumanTask::humanTaskDidExecute(HumanTask *task)
+void WorkHumanTask::humanTaskDidExecute(HumanTask* task)
 {
-    if(_humanComponent->getWorkplace() == nullptr)
+    if (_humanComponent->getWorkplace() == nullptr)
         terminateImmediately();
     else
     {
@@ -84,7 +66,7 @@ void WorkHumanTask::humanTaskDidExecute(HumanTask *task)
     }
 }
 
-void WorkHumanTask::humanTaskWillInteract(HumanTask *task)
+void WorkHumanTask::humanTaskWillInteract(HumanTask* task)
 {
     LOGF(F("HumanTaskWillInteract: interactionExecution return code: %1%") % _humanComponent->humanInteraction->executeInteraction(new HumanWorkInteraction()));
 }
--- Game/Game/WorkHumanTask.h
+++ Game/Game/WorkHumanTask.h
@@ -7,33 +7,29 @@
 //
 
 #pragma once
+
 #include <GlobalHeaders.h>
 #include "HumanTask.h"
 
 class HumanWorkInteraction;
 
-class WorkHumanTask : public HumanTask
+class WorkHumanTask
+    : public HumanTask
 {
 public:
-    ~WorkHumanTask();
+
     std::string getTaskName();
+
 protected:
     void onUpdate();
+
     Vector3 goTarget();
-    
-    
+
     void humanTaskDidExecute(HumanTask* task);
-//    
-//    void humanTaskWillGoToTarget(HumanTask* task);
-//    void humanTaskDidReachTarget(HumanTask* task);
-//    
+
     void humanTaskWillInteract(HumanTask* task);
-//    void humanTaskDidInteract(HumanTask* task);
-//    
-//    void humanTaskDidPause(HumanTask* task);
-//    void humanTaskDidResume(HumanTask* task);
-//    
-//    void humanTaskWillTerminateGracefully(HumanTask* task);
+
     void humanTaskWillTerminateImmediately(HumanTask* task);
+
     void humanTaskDidTerminate(HumanTask* task);
 };
